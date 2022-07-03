d839b1a06c87cf206ecc28be4235ad70b7cc780e
Noemi Gutierrez <noemigp0@gmail.com>
2022-07-02 22:07:08 -0500

    Adding homework

--- index.js
+++ index.js
@@ -1,49 +1,33 @@
 const express = require("express");
+const app = express();
+const fsPromises = require("fs").promises;
 
-const app = express()
-const  fsPromises  = require("fs").promises
+app.listen(8080, () => {
+  console.log("Ya estamos escuchando desde nuestro servidor express");
+});
 
-let path = "text.txt";
+/**
+ * Endpoints que sea /react y me regresen los alumnos que estan en el modulo
+ * de React.
+ */
 
-app.get("/leer-archivo", (request, response) => {    
-    leerArchivo(path)
-    .then(
-        result => {        
+const getReactStudents = async () => {
+  const koders = await fsPromises.readFile("koders.json", "utf-8");
+  const kodersJson = JSON.parse(koders); // que este parseado a json.
+  const reactModule = kodersJson.alumnos.filter((cv) => {
+    return cv.modulo === "React";
+  });
+  //
+  //console.log(reactModule)
+ return reactModule
+};
+
+
+
+
+app.get("/react-koders", (request, response) => {
+    getReactStudents().then(result => {
+        console.log(result)
         response.send(result)
-        }
-    ).catch(
-        err => 
-        response.send(err))   
+    }).catch(err => response.send(err))
 })
-
-//--> async/await --> try catch
-const leerArchivo = async ( ruta) => {
-
-    const readFile = await fsPromises.readFile(ruta, "utf-8")    
-    return readFile
-}
-
-// leerArchivo(path).then(
-//     result => console.log(result)
-// ).catch(err => console.log(err))
-
-
-
-
-// app.get("/hola", (request, response) => {
-//     response.write("Holaaa desde mi endpoint /hola")
-//     response.end()
-// })
-
-// app.post("/hola", (request, response) => {
-//     response.send("Estamos haciendo un post desde aqui")
-    
-// })
-
- app.listen(8080, () => {
-     console.log("Ya estamos escuchando desde nuestro servidor express")
- })
-
-//Ejercicio 
-//Hacer un endpoint, donde lea un archivo de texto
-
